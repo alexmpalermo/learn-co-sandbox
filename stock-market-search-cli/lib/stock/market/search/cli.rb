176126201792a1e76888b0de730f73f4720470f8
Alexandra Palermo <alexmpalermo@gmail.com>
2019-02-15 18:22:50 +0000

    first commit

---
+++ stock-market-search-cli/lib/stock/market/search/cli.rb
@@ -0,0 +1,12 @@
+require "stock/market/search/cli/version"
+
+module Stock
+  module Market
+    module Search
+      module Cli
+        class Error < StandardError; end
+        # Your code goes here...
+      end
+    end
+  end
+end
---
+++ stock-market-search-cli/lib/stock/market/search/cli/version.rb
@@ -0,0 +1,9 @@
+module Stock
+  module Market
+    module Search
+      module Cli
+        VERSION = "0.1.0"
+      end
+    end
+  end
+end
